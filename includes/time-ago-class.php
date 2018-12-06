7f76e688da64b552ab8ba137e0026fd3d44f20e1
Jack Lowrie <jack@4thwall.io>
2018-12-06 13:37:03 -0600

    add cases for each available option in time_ago_format_date

--- includes/time-ago-class.php
+++ includes/time-ago-class.php
@@ -15,7 +15,7 @@ class Time_Ago {
 
 
   // the meat of the plugin. Reformat the post date
-  public function time_ago_format_date($the_date, $d, $post) {
+  public function time_ago_format_date( $the_date, $d, $post ) {
     // Get the post id
     if ( is_int( $post) ) {
       $post_id = $post;
@@ -23,15 +23,36 @@ class Time_Ago {
       $post_id = $post->ID;
     }
 
-    $date_string = '';
-    $choice = 'default';
+    // get the format setting (user's choice)
+    $choice = 'no_minutes';
 
+
+    $date_string = '';
     switch ($choice) {
+
       case 'default':
         // subtract the current post date from the current time
-        $date_string = human_time_diff( get_the_time( 'U', $post_id ),
-                                        current_time('timestamp')
-                                      ) . ' ago';
+        $date_string = $this->time_ago_default_format( $post_id );
+        break;
+
+      case 'no_minutes':
+        $date_string = 'no_mins ';
+        break;
+
+      case 'no_hours':
+        $date_string = 'no_hours ';
+        break;
+
+      case 'no_days':
+        $date_string = 'no_days ';
+        break;
+
+      case 'no_weeks':
+        $date_string = 'no_weeks ';
+        break;
+
+      case 'no_months':
+        $date_string = 'no_months ';
         break;
 
       default:
@@ -40,12 +61,19 @@ class Time_Ago {
     }
 
     // make the string prettier
-    $date_string = $this->time_ago_prettify($date_string);
+    $date_string = $this->time_ago_prettify( $date_string );
     return $date_string;
   }
 
-  public function time_ago_prettify($datestring) {
+  public function time_ago_default_format( $post_id ) {
+    $datestring = human_time_diff( get_the_time( 'U', $post_id ), current_time('timestamp') );
+    return $datestring;
+  }
+
+  public function time_ago_prettify( $datestring ) {
+
+    $datestring .= ' ago';
     // makes 'minute' a full word. min -> minute, mins -> minutes
-    return str_replace('min', 'minute', $datestring);
+    return str_replace( 'min', 'minute', $datestring );
   }
 } // end of class
